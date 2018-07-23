a738c574658d66ae74946e53535a90598f6802dd
jmayda <34217931+jmayda@users.noreply.github.com>
2018-07-23 18:39:10 -0500

    Initial commit

---
+++ Section_1/PHP_page.php
@@ -0,0 +1,4 @@
+<?php
+    print("<h1>This page is rendered by the PHP server.</h1>");
+    print("<p>So Hello from PHP.</p>");
+?>
---
+++ Section_2/Echo_And_Print.php
@@ -0,0 +1,13 @@
+<?php
+    echo("<h1>Echoing out a heading</h1>");
+    print("<h1>Printing out a heading</h1>");
+    
+    echo "Old school style echo..."
+    print("<br/>" . 9*5);
+
+    //Expressions
+
+    echo(2+3);
+    print("<br/>" . 9*5);
+
+?>
